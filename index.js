e03fed40c7ac9dcbf837f9853b0485ee373d829d
Julian Gruber <julian@juliangruber.com>
2026-03-24 19:00:51 +0100

    refactor, large file upload works

--- index.js
+++ index.js
@@ -51,53 +51,57 @@ const source = new ReadableStream({
 //   console.log(`✅ Account funded and approved (tx: ${hash})`)
 // }
 
-let currentUploadStream
-let currentWriter
-let currentBytesWritten = 0
-const uploadSizeLimit = 1065353216
-// const uploadSizeLimit = bytes('1MB')
-const uploadPromises = []
-const splitter = new WritableStream({
-  async write (chunk) {
-    // FIXME
-    assert(chunk.length < uploadSizeLimit)
-    currentBytesWritten += chunk.length
+const createSplitter = () => {
+  let currentUploadStream
+  let currentWriter
+  let currentBytesWritten = 0
+  const uploadSizeLimit = 1065353216
+  // const uploadSizeLimit = bytes('1MB')
+  const uploadPromises = []
 
-    if (currentBytesWritten >= uploadSizeLimit) {
+  const splitter = new WritableStream({
+    async write (chunk) {
+      // FIXME
+      assert(chunk.length < uploadSizeLimit)
+      currentBytesWritten += chunk.length
+
+      if (currentBytesWritten >= uploadSizeLimit) {
+        currentWriter.releaseLock()
+        await currentUploadStream.writable.close()
+        currentUploadStream = null
+      }
+
+      if (!currentUploadStream) {
+        currentBytesWritten = chunk.length
+        currentUploadStream = new TransformStream()
+        currentWriter = currentUploadStream.writable.getWriter()
+        uploadPromises.push(synapse.storage.upload(currentUploadStream.readable))
+        console.log(`pieces: ${uploadPromises.length}`)
+      }
+
+      await currentWriter.write(chunk)
+    },
+
+    async close () {
+      // TODO: This assumes at least one chunk
       currentWriter.releaseLock()
       await currentUploadStream.writable.close()
-      currentUploadStream = null
     }
+  })
 
-    if (!currentUploadStream) {
-      currentBytesWritten = chunk.length
-      currentUploadStream = new TransformStream()
-      currentWriter = currentUploadStream.writable.getWriter()
-      uploadPromises.push(synapse.storage.upload(currentUploadStream.readable))
-      console.log(`pieces: ${uploadPromises.length}`)
-    }
-    
-    await currentWriter.write(chunk)
-  },
+  return { splitter, uploadPromises }
+}
 
-  async close () {
-    // TODO: This assumes at least one chunk
-    currentWriter.releaseLock()
-    await currentUploadStream.writable.close()
-  }
-})
+const uploadLfs = async data => {
+  const { splitter, uploadPromises } = createSplitter(data)
+  await data.pipeTo(splitter)
+  const uploadResults = await Promise.all(uploadPromises)
+  return uploadResults
+}
 
-await source.pipeTo(splitter)
-const uploadResults = await Promise.all(uploadPromises)
-
+const uploadResults = await uploadLfs(source)
 console.log('PieceCIDs:')
 for (const { pieceCid } of uploadResults) {
   console.log(`- ${pieceCid}`)
 }
 console.log(`Upload took ${Math.floor((new Date() - startUpload)/1000/60)} minutes`)
-
-const startDownload = new Date()
-console.log('downloading...')
-await Promise.all(uploadResults.map(({ pieceCid }) => synapse.storage.download(pieceCid)))
-console.log(`✅ Download successful!`)
-console.log(`download took ${Math.floor((new Date() - startDownload) / 1000 / 60)} minutes`)
